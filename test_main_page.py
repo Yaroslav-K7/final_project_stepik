8373641befde2dd141a31002e845068f68fe14bc
Yaroslav <yaroslav.kuznetsov1994@gmail.com>
2020-04-02 21:31:52 +0300

    test

--- test_main_page.py
+++ test_main_page.py
@@ -1,23 +1,34 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+import time
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
-
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
-    page.should_be_login_link()
+    time.sleep(5)
+    print(browser.current_url, "1")
+    page.go_to_login_page()   # выполняем метод страницы - переходим на страницу логина
+    print(browser.current_url, "2")
+    login_page = LoginPage(browser, browser.current_url)
+    print(browser.current_url, "3")
+    login_page.should_be_login_page()
+    time.sleep(5)
 
 
-def test_guest_should_see_login_and_register_dorm(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+# def test_guest_should_see_login_link(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+#
+#
+# def test_guest_should_see_login_and_register_dorm(browser):
+#     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_page()
+#
+
+
